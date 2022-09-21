a8a172ba7e03b433454a739866a8f4b3f1d69d0d
blich student <student-cyber@blich.co.il>
2022-09-21 14:30:34 +0300

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,19 @@
+﻿class ConsoleApp2
+{
+    public static void Main()
+    {
+        int class_1, class_2, class_3, buses, total_students;
+        Console.WriteLine("Enter the number of students in class #1: ");
+        class_1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the number of students in class #2: ");
+        class_2 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the number of students in class #3: ");
+        class_3 = int.Parse(Console.ReadLine());
+        total_students = class_1 + class_2 + class_3;
+        if (total_students % 40 == 0)
+            buses = total_students / 40;
+        else
+            buses = total_students / 40 + 1;
+        Console.WriteLine("Your total busses is {0}!", buses);
+    }
+}
